9d2a7acb30b138831beb3a96c1196786c99bdab2
eriguchi.aoi <eriguchi.aoi@lib-gate.co.jp>
2020-07-15 20:48:45 +0900

    itex通信_特典動画変更、php include

--- index.php
+++ index.php
@@ -1,61 +1,25 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-        <link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
-        <link rel="stylesheet" href="./dst/css/index.css">
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131225464-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+		<head>
+				<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+				<link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
+				<link rel="stylesheet" href="./dst/css/index.css">
+				<!-- Global site tag (gtag.js) - Google Analytics -->
+				<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131225464-1"></script>
+				<script>
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
 
-            gtag('config', 'UA-131225464-1');
-        </script>
-        <title>Lib-gate analytics</title>
-    </head>
-    <body>
-<?php if (strpos($_SERVER['QUERY_STRING'], 'bad-ui') !== false) : ?>
-        <header class="badui-header">
-            BAD UI
-        </header>
-
-        <h2 class="badui-title">
-            いいねありがとうございました。
-        </h2>
-        <div class="wrapper" style="padding: 16px;">
-            <a class="twitter-moment" href="https://twitter.com/i/moments/1102519851556327424?ref_src=twsrc%5Etfw">UIってむずかしいね</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
-        </div>
-<?php elseif (strpos($_SERVER['QUERY_STRING'], 'itex') !== false) : ?>
-        <header class="itex-header">
-            ITEX通信
-        </header>
-
-        <h2 class="itex-title">
-            いいねありがとうございました。
-        </h2>
-
-        <div class="wrapper itex">
-            <p>こちらの記事の内容はSlackの<a href="https://app.slack.com/client/TPXECNKK8/CPMAZMDUZ">社内通知チャンネル</a>にも配信しています。<br>コメントはチャンネル内にていただけると嬉しいです！</p>
-            <p style="text-align: center;">★いいねをしてくれた方限定　特典映像★</p>
-            <div class="youtube">
-                <iframe src="https://www.youtube.com/embed/MXBgOUqJBaA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <pre>みなさん
-こんにちは 小山です。
-
-こちらはフットサルイベントの映像ですが、
-本来は普通に楽しい行事なので是非参加してください。
-
-そして私がこの映像を通してお伝えしたいことは、
-「準備運動はしっかりしよう」ということです。
-
-みなさんも準備運動をして、楽しいフットサルをしてください。
-
-次回の特典映像でお会いしましょう。
-            </pre>
-        </div>
-<?php endif ?>
-    </body>
+						gtag('config', 'UA-131225464-1');
+				</script>
+				<title>Lib-gate analytics</title>
+		</head>
+		<body>
+<?php if (strpos($_SERVER['QUERY_STRING'], 'bad-ui') !== false) {
+	include("./include/bad-ui.html");
+} elseif (strpos($_SERVER['QUERY_STRING'], 'itex') !== false) {
+	include("./include/itex.html");
+} ?>
+		</body>
 </html>
